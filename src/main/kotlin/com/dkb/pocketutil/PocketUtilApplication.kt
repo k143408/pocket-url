242031134badda5876538db9d6fb5f7a37dffc10
Jibran Tariq <jibran.tariq@spark.net>
2022-09-13 19:26:21 +0200

    feat(init): Initialize project. - Adopted Spring boot with kotlin

---
+++ src/main/kotlin/com/dkb/pocketutil/PocketUtilApplication.kt
@@ -0,0 +1,11 @@
+package com.dkb.pocketutil
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PocketUtilApplication
+
+fun main(args: Array<String>) {
+    runApplication<PocketUtilApplication>(*args)
+}
---
+++ src/test/kotlin/com/dkb/pocketutil/PocketUtilApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.dkb.pocketutil
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PocketUtilApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
